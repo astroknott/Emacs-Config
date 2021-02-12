1b03d4a8098c5540bd206571c09afb07b506baf9
astroknott <aaronknott8@gmail.com>
2021-02-12 10:52:56 -0800

    changed name of config to init.el

---
+++ init.el
@@ -0,0 +1,303 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(avy-timeout-seconds 0.2)
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
+ '(compilation-message-face 'default)
+ '(cua-global-mark-cursor-color "#2aa198")
+ '(cua-normal-cursor-color "#657b83")
+ '(cua-overwrite-cursor-color "#b58900")
+ '(cua-read-only-cursor-color "#859900")
+ '(custom-enabled-themes '(solarized-light))
+ '(custom-safe-themes
+   '("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "465f04ecb7486d45ed9e186588e16b0b2e7a8fc04d7d355d0ba9c0c4ea3fb6de" "0754c176c3f850e1f90e177e283247749a1ac688faebf05b6016343cb3f00064" "eb3ef63dc31fbebb37df3646fbc1e07f25159d19bb1cade2a80000a2764690de" "27a1dd6378f3782a593cc83e108a35c2b93e5ecc3bd9057313e1d88462701fcd" "0feb7052df6cfc1733c1087d3876c26c66410e5f1337b039be44cb406b6187c6" "0fffa9669425ff140ff2ae8568c7719705ef33b7a927a0ba7c5e2ffcfac09b75" "0bff60fb779498e69ea705825a2ca1a5497a4fccef93bf3275705c2d27528f2f" "d9766ec8d1dca35ad9392d212da441517800028b585d187f08ea44ce1c84ebdf" "ffba0482d3548c9494e84c1324d527f73ea4e43fff8dfd0e48faa8fc6d5c2bc7" "834cbeacb6837f3ddca4a1a7b19b1af3834f36a701e8b15b628cad3d85c970ff" "76c5b2592c62f6b48923c00f97f74bcb7ddb741618283bdb2be35f3c0e1030e3" "bd82c92996136fdacbb4ae672785506b8d1d1d511df90a502674a51808ecc89f" "8f5b54bf6a36fe1c138219960dd324aad8ab1f62f543bed73ef5ad60956e36ae" "d0fd069415ef23ccc21ccb0e54d93bdbb996a6cce48ffce7f810826bb243502c" "cbd85ab34afb47003fa7f814a462c24affb1de81ebf172b78cb4e65186ba59d2" "c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "285d1bf306091644fb49993341e0ad8bafe57130d9981b680c1dbd974475c5c7" "be9645aaa8c11f76a10bcf36aaf83f54f4587ced1b9b679b55639c87404e2499" "1ed5c8b7478d505a358f578c00b58b430dde379b856fbcb60ed8d345fc95594e" "0ad7f1c71fd0289f7549f0454c9b12005eddf9b76b7ead32a24d9cb1d16cbcbd" "6231254e74298a1cf8a5fee7ca64352943de4b495e615c449e9bb27e2ccae709" "3e3a1caddeee4a73789ff10ba90b8394f4cd3f3788892577d7ded188e05d78f4" "845103fcb9b091b0958171653a4413ccfad35552bc39697d448941bcbe5a660d" "6bacece4cf10ea7dd5eae5bfc1019888f0cb62059ff905f37b33eec145a6a430" "a339f231e63aab2a17740e5b3965469e8c0b85eccdfb1f9dbd58a30bdad8562b" "d71aabbbd692b54b6263bfe016607f93553ea214bc1435d17de98894a5c3a086" "9b01a258b57067426cc3c8155330b0381ae0d8dd41d5345b5eddac69f40d409b" "1526aeed166165811eefd9a6f9176061ec3d121ba39500af2048073bea80911e" "3577ee091e1d318c49889574a31175970472f6f182a9789f1a3e9e4513641d86" "a83f05e5e2f2538376ea2bfdf9e3cd8b7f7593b16299238c1134c1529503fa88" "bc836bf29eab22d7e5b4c142d201bcce351806b7c1f94955ccafab8ce5b20208" default))
+ '(dropbox-access-token
+   "sl.Al31qi5kH4IU3PhIghfL1Kpm5RZCIXhfZA0W-bII3kaaSbYOnRsj1lHH05G2End1meB-Rt0evfcY5vAs3nPPNgceMc5GutACmTZ1ixz7YtuA9XISJ5uFv-M-HbUix1SPlqaGRSk")
+ '(fci-rule-color "#6272a4")
+ '(grep-find-ignored-files
+   '(".#*" "*.o" "*~" "*.bin" "*.lbin" "*.so" "*.a" "*.ln" "*.blg" "*.bbl" "*.elc" "*.lof" "*.glo" "*.idx" "*.lot" "*.fmt" "*.tfm" "*.class" "*.fas" "*.lib" "*.mem" "*.x86f" "*.sparcf" "*.dfsl" "*.pfsl" "*.d64fsl" "*.p64fsl" "*.lx64fsl" "*.lx32fsl" "*.dx64fsl" "*.dx32fsl" "*.fx64fsl" "*.fx32fsl" "*.sx64fsl" "*.sx32fsl" "*.wx64fsl" "*.wx32fsl" "*.fasl" "*.ufsl" "*.fsl" "*.dxl" "*.lo" "*.la" "*.gmo" "*.mo" "*.toc" "*.aux" "*.cp" "*.fn" "*.ky" "*.pg" "*.tp" "*.vr" "*.cps" "*.fns" "*.kys" "*.pgs" "*.tps" "*.vrs" "*.pyc" "*.pyo" "*.lock"))
+ '(helm-boring-file-regexp-list
+   '("\\.o$" "~$" "\\.bin$" "\\.lbin$" "\\.so$" "\\.a$" "\\.ln$" "\\.blg$" "\\.bbl$" "\\.elc$" "\\.lof$" "\\.glo$" "\\.idx$" "\\.lot$" "\\.svn\\(/\\|$\\)" "\\.hg\\(/\\|$\\)" "\\.git\\(/\\|$\\)" "\\.bzr\\(/\\|$\\)" "CVS\\(/\\|$\\)" "_darcs\\(/\\|$\\)" "_MTN\\(/\\|$\\)" "\\.fmt$" "\\.tfm$" "\\.class$" "\\.fas$" "\\.lib$" "\\.mem$" "\\.x86f$" "\\.sparcf$" "\\.dfsl$" "\\.pfsl$" "\\.d64fsl$" "\\.p64fsl$" "\\.lx64fsl$" "\\.lx32fsl$" "\\.dx64fsl$" "\\.dx32fsl$" "\\.fx64fsl$" "\\.fx32fsl$" "\\.sx64fsl$" "\\.sx32fsl$" "\\.wx64fsl$" "\\.wx32fsl$" "\\.fasl$" "\\.ufsl$" "\\.fsl$" "\\.dxl$" "\\.lo$" "\\.la$" "\\.gmo$" "\\.mo$" "\\.toc$" "\\.aux$" "\\.cp$" "\\.fn$" "\\.ky$" "\\.pg$" "\\.tp$" "\\.vr$" "\\.cps$" "\\.fns$" "\\.kys$" "\\.pgs$" "\\.tps$" "\\.vrs$" "\\.pyc$" "\\.pyo$" "package.json" "package-lock.json" "yarn.lock" "*.ts"))
+ '(helm-completion-style 'emacs)
+ '(helm-ff-skip-boring-files t)
+ '(helm-grep-ignored-files
+   '(".#*" "*.o" "*~" "*.bin" "*.lbin" "*.so" "*.a" "*.ln" "*.blg" "*.bbl" "*.elc" "*.lof" "*.glo" "*.idx" "*.lot" "*.fmt" "*.tfm" "*.class" "*.fas" "*.lib" "*.mem" "*.x86f" "*.sparcf" "*.dfsl" "*.pfsl" "*.d64fsl" "*.p64fsl" "*.lx64fsl" "*.lx32fsl" "*.dx64fsl" "*.dx32fsl" "*.fx64fsl" "*.fx32fsl" "*.sx64fsl" "*.sx32fsl" "*.wx64fsl" "*.wx32fsl" "*.fasl" "*.ufsl" "*.fsl" "*.dxl" "*.lo" "*.la" "*.gmo" "*.mo" "*.toc" "*.aux" "*.cp" "*.fn" "*.ky" "*.pg" "*.tp" "*.vr" "*.cps" "*.fns" "*.kys" "*.pgs" "*.tps" "*.vrs" "*.pyc" "*.pyo" "yarn.lock" "package-lock.json" "*.spec.js" "reducers.js" "selectors.js" "*.ts"))
+ '(helm-rg-default-directory 'git-root)
+ '(helm-rg-ripgrep-executable "rg")
+ '(helm-swoop-min-overlay-length 2)
+ '(highlight-changes-colors '("#d33682" "#6c71c4"))
+ '(highlight-symbol-colors
+   '("#eef6d970af00" "#cef5e0cccfbb" "#fd55c91cb29c" "#dadbd2efdc17" "#e0a3de02afa1" "#f84bcba1ad99" "#d28bd9ebdf8a"))
+ '(highlight-symbol-foreground-color "#586e75")
+ '(highlight-tail-colors
+   '(("#eee8d5" . 0)
+     ("#b3c34d" . 20)
+     ("#6ccec0" . 30)
+     ("#74adf5" . 50)
+     ("#e1af4b" . 60)
+     ("#fb7640" . 70)
+     ("#ff699e" . 85)
+     ("#eee8d5" . 100)))
+ '(hl-bg-colors
+   '("#e1af4b" "#fb7640" "#ff6849" "#ff699e" "#8d85e7" "#74adf5" "#6ccec0" "#b3c34d"))
+ '(hl-fg-colors
+   '("#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3"))
+ '(hl-paren-colors '("#2aa198" "#b58900" "#268bd2" "#6c71c4" "#859900"))
+ '(hl-todo-keyword-faces
+   '(("TODO" . "#dc752f")
+     ("NEXT" . "#dc752f")
+     ("THEM" . "#2d9574")
+     ("PROG" . "#3a81c3")
+     ("OKAY" . "#3a81c3")
+     ("DONT" . "#f2241f")
+     ("FAIL" . "#f2241f")
+     ("DONE" . "#42ae2c")
+     ("NOTE" . "#b1951d")
+     ("KLUDGE" . "#b1951d")
+     ("HACK" . "#b1951d")
+     ("TEMP" . "#b1951d")
+     ("FIXME" . "#dc752f")
+     ("XXX+" . "#dc752f")
+     ("\\?\\?\\?+" . "#dc752f")))
+ '(jdee-db-active-breakpoint-face-colors (cons "#1E2029" "#bd93f9"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1E2029" "#50fa7b"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1E2029" "#565761"))
+ '(js-indent-level 2)
+ '(js-jsx-indent-level 2)
+ '(js-jsx-syntax t)
+ '(js-switch-indent-offset 2)
+ '(lsp-ui-doc-border "#586e75")
+ '(mode-line-percent-position nil)
+ '(nrepl-message-colors
+   '("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3"))
+ '(objed-cursor-color "#ff5555")
+ '(org-agenda-clockreport-parameter-plist '(:link t :maxlevel 3))
+ '(org-agenda-files nil)
+ '(org-agenda-start-with-clockreport-mode t)
+ '(org-capture-templates
+   '(("t" "test template" entry
+      (file "main.org")
+      "Hello test")))
+ '(org-clock-clocktable-default-properties '(:maxlevel 3))
+ '(org-hide-leading-stars t)
+ '(org-refile-targets '(("archive.org" :tag . "")))
+ '(package-archives
+   '(("melpa" . "https://melpa.org/packages/")
+     ("gnu" . "https://elpa.gnu.org/packages/")))
+ '(package-selected-packages
+   '(elfeed powerline smart-mode-line vue-mode moe-theme northcode-theme pastelmac-theme dropbox web-mode gdscript-mode string-inflection helm-rg multiple-cursors company rjsx-mode tide avy docker magit ido-vertical-mode hydra helm-swoop helm-projectile))
+ '(pcmpl-ssh-config-file "/home/aaronk/.ssh/config")
+ '(pcmpl-ssh-known-hosts-file "/home/aaronk/.ssh/known_hosts")
+ '(pdf-view-midnight-colors (cons "#f8f8f2" "#282a36"))
+ '(pos-tip-background-color "#eee8d5")
+ '(pos-tip-foreground-color "#586e75")
+ '(rustic-ansi-faces
+   ["#282a36" "#ff5555" "#50fa7b" "#f1fa8c" "#61bfff" "#ff79c6" "#8be9fd" "#f8f8f2"])
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#eee8d5" 0.2))
+ '(term-default-bg-color "#fdf6e3")
+ '(term-default-fg-color "#657b83")
+ '(vc-annotate-background "#282a36")
+ '(vc-annotate-background-mode nil)
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#50fa7b")
+    (cons 40 "#85fa80")
+    (cons 60 "#bbf986")
+    (cons 80 "#f1fa8c")
+    (cons 100 "#f5e381")
+    (cons 120 "#face76")
+    (cons 140 "#ffb86c")
+    (cons 160 "#ffa38a")
+    (cons 180 "#ff8ea8")
+    (cons 200 "#ff79c6")
+    (cons 220 "#ff6da0")
+    (cons 240 "#ff617a")
+    (cons 260 "#ff5555")
+    (cons 280 "#d45558")
+    (cons 300 "#aa565a")
+    (cons 320 "#80565d")
+    (cons 340 "#6272a4")
+    (cons 360 "#6272a4")))
+ '(vc-annotate-very-old-color nil)
+ '(web-mode-markup-indent-offset 2)
+ '(weechat-color-list
+   '(unspecified "#fdf6e3" "#eee8d5" "#a7020a" "#dc322f" "#5b7300" "#859900" "#866300" "#b58900" "#0061a8" "#268bd2" "#a00559" "#d33682" "#007d76" "#2aa198" "#657b83" "#839496"))
+ '(xterm-color-names
+   ["#eee8d5" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#073642"])
+ '(xterm-color-names-bright
+   ["#fdf6e3" "#cb4b16" "#93a1a1" "#839496" "#657b83" "#6c71c4" "#586e75" "#002b36"]))
+
+;; removes file-lock mechanism -- useful when having issues with hotreload/webpack
+(setq create-lockfiles nil)
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(helm-rg-directory-cmd-face ((t (:background "magenta" :foreground "white" :weight ultra-bold))))
+ '(helm-rg-file-match-face ((t (:foreground "dark violet" :underline nil))))
+ '(helm-rg-line-number-match-face ((t (:foreground "light coral" :underline nil))))
+ '(helm-rg-match-text-face ((t (:background "magenta" :foreground "white"))))
+ '(helm-rg-preview-line-highlight ((t (:background "light cyan" :weight ultra-bold))))
+ '(web-mode-current-column-highlight-face ((t (:background "papaya whip"))))
+ '(web-mode-current-element-highlight-face ((((class color) (min-colors 89)) (:underline t :weight unspecified :background "#eee8d5")))))
+
+(require 'dropbox)
+
+;; completion framwork
+(require 'helm)
+(helm-mode 1)
+(require 'helm-projectile)
+(require 'projectile)
+(require 'helm-config)
+(use-package all-the-icons)
+(defalias 'yes-or-no-p 'y-or-n-p)
+(setq org-refile-use-outline-path 'file)
+(setq org-outline-path-complete-in-steps t)
+
+;; mode line stuff
+;; (sml/setup)
+;; (setq sml/theme 'light)
+(require 'powerline)
+(powerline-default-theme)
+
+(projectile-global-mode)
+
+(subword-mode)
+
+;; web mode stuff
+(setq web-mode-enable-current-element-highlight t)
+(setq web-mode-enable-current-column-highlight t)
+(add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+
+(require 'org)
+
+;; shell customizations
+(setq comint-scroll-to-bottom-on-input t)
+(setq comint-scroll-to-bottom-on-output t)
+
+(add-hook 'org-mode-hook (lambda () (org-autolist-mode)))
+(add-hook 'org-mode-hook 'org-superstar-mode)
+(define-key org-mode-map "M-p" 'org-shiftup)
+(define-key org-mode-map "M-n" 'org-shiftdown)
+
+(global-hl-line-mode)
+(helm-projectile-on)
+(show-smartparens-global-mode)
+(smartparens-global-mode 1)
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+
+;; shortcut keys
+(global-set-key (kbd "M-+") 'shift-number-up)
+(global-set-key (kbd "M-_") 'shift-number-down)
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
+(global-set-key (kbd "C-s") 'helm-swoop)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-c rg") 'helm-rg)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x r b") 'helm-bookmarks)
+(global-set-key (kbd "C-;") 'avy-goto-char-timer)
+(global-set-key (kbd "C-:") 'avy-copy-line)
+(global-set-key (kbd "C-'") 'avy-copy-region)
+(global-set-key (kbd "C-M-l") 'avy-goto-line)
+(global-set-key (kbd "C-c f") 'helm-projectile-find-file)
+(global-set-key (kbd "C-c s") 'projectile-run-shell)
+(global-unset-key (kbd "C-<down-mouse-1>"))
+(global-set-key (kbd "M-c") 'fence-edit-code-at-point)
+(global-set-key (kbd "C-_") 'string-inflection-kebab-case)
+(global-set-key (kbd "M-RET") 'eshell)
+
+(require 'magit)
+(define-key magit-mode-map "@" 'docker)
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+(add-hook 'typescript-mode-hook 'setup-tide-mode)
+(add-hook 'js-mode-hook 'setup-tide-mode)
+(add-hook 'rjsx-mode-hook 'setup-tide-mode)
+
+;; Company
+(global-company-mode 1)
+;; remove ui stuff
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+;; general completion
+(require 'ido)
+(ido-mode t)
+(require 'hydra)
+(defhydra aarons/smerge-hydra (:color pink :hint nil)
+  "
+^Navigate^       ^Selection^
+^^^^^^^^----------------------
+_n_: next       _m_: mine
+_N_: next-file  _o_: other
+_p_: prev       _a_: all
+  ^^            _u_: upper
+  ^^            _l_: lower
+"
+  ("n" smerge-next)
+  ("N" smerge-vc-next-conflict)
+  ("p" smerge-prev)
+  ("m" smerge-keep-mine)
+  ("o" smerge-keep-other)
+  ("a" smerge-keep-all)
+  ("u" smerge-keep-upper)
+  ("l" smerge-keep-lower)
+  ("q" nil "quit" :color blue))
+
+;; setup windmove with vim keybindings
+(when (fboundp 'windmove-default-keybindings)
+  (windmove-default-keybindings))
+
+(global-set-key (kbd "M-h") 'windmove-left)
+(global-set-key (kbd "M-j") 'windmove-down)
+(global-set-key (kbd "M-k") 'windmove-up)
+(global-set-key (kbd "M-l") 'windmove-right)
+
+(setq typescript-indent-level 2)
+(put 'dired-find-alternate-file 'disabled nil)
+
+(add-hook 'rjsx-mode-hook (lambda () (modify-syntax-entry ?` "\"" js-mode-syntax-table)))
+
+(setq
+ styled-component-start
+ (rx-to-string '(: (1+ (and (+ word) (0+ "\.") (0+ "(" (+ alpha) ")"))) "`" eol)))
+(setq
+ styled-component-end
+ (rx-to-string '(: "`;" eol)))
+(setq
+ fence-edit-blocks `((,styled-component-start ,styled-component-end)))
+(setq fence-edit-default-mode 'scss-mode)
+
+
+(add-to-list 'load-path "~/.emacs.d/customPackages/fence-edit/")
+
+(load "fence-edit")
+
+(yas-global-mode t)
+
+(load-theme 'solarized-light);
